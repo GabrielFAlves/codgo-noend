912e3f633b5714c7c04bb84215c61db1d5d548d7
GABRIEL FIRMAMENTO ALVES <202302858112@alunos.ibmec.edu.br>
2023-06-14 07:55:41 -0300

    login2

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -12,51 +12,41 @@ function Login(){
                 <img src={logo} className='logologo' alt="logo" />
                 </div>
             <div className='Container'>
-                            <div className='Chad-image'>
-                                <img src= {img} alt='Imagem ilustrativa' title='Imagem ilustrativa ' />
-                                </div>
-                            <div className='Chad'>
-                                <form action="#">
-                                    <div className='Chad-header'>
-                                        <div className='Chad-title'>
-                                            <h1 className=''>Login</h1>
-                                        </div>
-                                    </div>
-                                    
-                                    <div className='Input-group'>
-                                        <div className='Input-box'>
-                                            <label for='user'>Usuário</label>
-                                            <input id='user' type='text' name='user' placeholder='Digite seu nome' required/>
-                                        </div>
-
-<<<<<<< HEAD
-                                        <div className='Input-box'>
-                                            <label for='password'>Senha</label>
-                                            <input id='password' type='password' name='password' placeholder='Digitar senha' required/>
-                                        </div>
-                                        
-                                        <div>
-                                            <a href="#"  id='esqueceu_senha'>Esqueceu sua senha?</a>
-                                        </div>
-                                    </div>
-
-                                    <div className='Send-button'>
-                                            <button><a href='#'>Login</a></button>
-                                    </div>
-                                </form>
+                <div className='Chad-image'>
+                    <img src= {img} alt='Imagem ilustrativa' title='Imagem ilustrativa ' />
+                </div>
+                <div className='Chad'>
+                    <form action="#">
+                        <div className='Chad-header'>
+                            <div className='Chad-title'>
+                                <h1 className=''>Login</h1>
                             </div>
                         </div>
-=======
-                        <form method='POST'>
-                            <input type="text" name='nome' placeholder='nome de usuario' autoFocus />
-                            <input type="password" name='senha' placeholder='sua senha' />
-                            <input type="submit" value="entrar" id='b-entrar' />
-                        </form>
-                        <a href=''>Esqueceu sua senha???</a>
-                    </div>
-                </section>
+                        
+                        <div className='Input-group'>
+                            <div className='Input-box'>
+                                <label for='user'>Usuário</label>
+                                <input id='user' type='text' name='user' placeholder='Digite seu nome' required/>
+                            </div>
+
+
+                            <div className='Input-box'>
+                                <label for='password'>Senha</label>
+                                <input id='password' type='password' name='password' placeholder='Digitar senha' required/>
+                            </div>
+                            
+                            <div>
+                                <a href="#"  id='esqueceu_senha'>Esqueceu sua senha?</a>
+                            </div>
+                        </div>
+
+                        <div className='Send-button'>
+                                <button><a href='#'>Login</a></button>
+                        </div>
+                    </form>          
+                </div>
             </div>
->>>>>>> 820fcc07f49056c9d211618a6925a5d5ba6be7e4
+
             </LoginDiv>
         </>
     );
